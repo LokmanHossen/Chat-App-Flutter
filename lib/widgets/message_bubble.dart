c7bf38f43257c9e85cf2025f7244e2f368e47aa2
Lokman Hossen <lokmanhossen230@gmail.com>
2024-10-21 03:29:33 +0600

    Update

--- lib/widgets/message_bubble.dart
+++ lib/widgets/message_bubble.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 class MessageBubble extends StatelessWidget {
   const MessageBubble.first({
     super.key,
-    this.userImage,
-    this.username,
+    required this.userImage,
+    required this.username,
     required this.message,
     required this.isMe,
   }) : isFirstInbSequence = true;
@@ -37,6 +37,7 @@ class MessageBubble extends StatelessWidget {
                 userImage!,
               ),
               backgroundColor: theme.colorScheme.primary.withAlpha(180),
+              radius: 23,
             ),
           ),
         Container(
@@ -45,53 +46,60 @@ class MessageBubble extends StatelessWidget {
             mainAxisAlignment:
                 isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
             children: [
-              if (isFirstInbSequence) const SizedBox(height: 18),
-              if (username != null)
-                Padding(
-                  padding: const EdgeInsets.only(
-                    left: 13,
-                    right: 13,
-                  ),
-                  child: Text(
-                    username!,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black87,
+              Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  if (isFirstInbSequence) const SizedBox(height: 18),
+                  if (username != null)
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 13,
+                        right: 13,
+                      ),
+                      child: Text(
+                        username!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              //spech box surround the message
-              Container(
-                decoration: BoxDecoration(
-                  color: isMe
-                      ? Colors.grey[300]
-                      : theme.colorScheme.secondary.withAlpha(200),
-                  borderRadius: BorderRadius.only(
-                    topLeft: !isMe && isFirstInbSequence
-                        ? Radius.zero
-                        : const Radius.circular(12),
-                    bottomLeft: const Radius.circular(12),
-                    bottomRight: const Radius.circular(12),
-                  ),
-                ),
-                constraints: const BoxConstraints(maxWidth: 200),
-                padding: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 14,
-                ),
-                margin: const EdgeInsets.symmetric(
-                  vertical: 4,
-                  horizontal: 14,
-                ),
-                child: Text(
-                  message,
-                  style: TextStyle(
-                    height: 1.3,
-                    color:
-                        isMe ? Colors.black87 : theme.colorScheme.onSecondary,
-                  ),
-                ),
-              )
+                  Container(
+                    decoration: BoxDecoration(
+                      color: isMe
+                          ? Colors.grey[300]
+                          : theme.colorScheme.secondary.withAlpha(200),
+                      borderRadius: BorderRadius.only(
+                        topLeft: !isMe && isFirstInbSequence
+                            ? Radius.zero
+                            : const Radius.circular(12),
+                        bottomLeft: const Radius.circular(12),
+                        bottomRight: const Radius.circular(12),
+                      ),
+                    ),
+                    constraints: const BoxConstraints(maxWidth: 200),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 14,
+                    ),
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 4,
+                      horizontal: 12,
+                    ),
+                    child: Text(
+                      message,
+                      style: TextStyle(
+                        height: 1.3,
+                        color: isMe
+                            ? Colors.black87
+                            : theme.colorScheme.onSecondary,
+                      ),
+                      softWrap: true,
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
         )
