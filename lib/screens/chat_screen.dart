ecac80059cd9e83ed43d85ccdd17a547c100a792
Lokman Hossen <lokmanhossen230@gmail.com>
2024-10-21 18:56:03 +0600

    PushNotification

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -16,8 +16,9 @@ class _ChatScreenState extends State<ChatScreen> {
     final fcm = FirebaseMessaging.instance;
     await fcm.requestPermission();
 
-    final token = await fcm.getToken();
-    print('Token: $token');
+    // final token = await fcm.getToken();
+    // print('Token: $token');
+    fcm.subscribeToTopic('chat');
   }
 
   @override
